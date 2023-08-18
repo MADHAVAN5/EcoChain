be5d916d6f929fca157c7c739688dc9406e001c6
MADAVAN K <kmadavan018@gmail.com>
2023-08-18 17:16:17 +0530

    Month Model API | Flask

--- ml-backend/views/views.py
+++ ml-backend/views/views.py
@@ -3,11 +3,20 @@ import numpy as np
 import tensorflow as tf
 
 from app import *
+weekmodel = tf.keras.models.load_model("models/weekPrediction.h5", compile=False)
+monthmodel = tf.keras.models.load_model("models/monthModel.h5", compile=False)
 
-@app.route('/', methods = ['GET', 'POST'])
-def home():
-    model = tf.keras.models.load_model("models/weekPrediction.h5", compile=False)
+@app.route('/week', methods = ['GET', 'POST'])
+def week():
     data = request.json['data']
     test = np.array(data).astype(np.float32)
-    pred = model.predict(test.reshape(1,7))
+    pred = weekmodel.predict(test.reshape(1,7))
+    return jsonify({'data': pred.tolist()})
+
+@app.route('/month', methods = ['GET', 'POST'])
+def month():
+    
+    data = request.json['data']
+    test = np.array(data).astype(np.float32)
+    pred = monthmodel.predict(test.reshape(1,30))
     return jsonify({'data': pred.tolist()})
