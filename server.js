53009543686ce1fd6f0aeb0c54df417f8a9c9b33
MADAVAN K <kmadavan018@gmail.com>
2023-08-18 14:12:22 +0530

    Register & Login | UI

--- server.js
+++ server.js
@@ -5,17 +5,21 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+//Routes
+
+
 app.use(express.static('public'))
 app.use(express.urlencoded({
     extended: true
-  }))
+}))
+
 
 app.use(bodyParser.json());
 app.set('view engine', 'ejs')
 
 
 app.get("/",(req,res)=>{
-    res.render('index')
+    res.render('login')
 })
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
