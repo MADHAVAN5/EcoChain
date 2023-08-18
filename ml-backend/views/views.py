2f2754dbab532b830661e210573f72993d846883
MADAVAN K <kmadavan018@gmail.com>
2023-08-18 16:19:29 +0530

    bug fix

--- ml-backend/views/views.py
+++ ml-backend/views/views.py
@@ -4,11 +4,10 @@ import tensorflow as tf
 
 from app import *
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def home():
     model = tf.keras.models.load_model("models/weekPrediction.h5", compile=False)
     data = request.json['data']
-    print(data)
-    test = np.array([12.31,7.44,5.23,5.65,4.67,6.75,9.34]).astype(np.float32)
+    test = np.array(data).astype(np.float32)
     pred = model.predict(test.reshape(1,7))
-    return jsonify({'data': pred})
+    return jsonify({'data': pred.tolist()})
