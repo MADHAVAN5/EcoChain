ca847cffe5e218ff489feae6ba9f9d4b2cd1c64b
MADAVAN K <kmadavan018@gmail.com>
2023-08-18 11:54:02 +0530

    node structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+
+app.get("/",(req,res)=>{
+    res.send('Home Page')
+})
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
