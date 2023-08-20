fcaaedfdec3312816f5c7bb3935f0a2eb9e6764c
priyankakannan0229 <65155327+MUKILAN-2003@users.noreply.github.com>
2023-08-20 13:56:27 +0530

    changes of token

--- public/js/app.js
+++ public/js/app.js
@@ -552,6 +552,101 @@ App = {
 
         window.location.href = '/dashboard'
     },
+    setTokenPrice: async () => {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+        } else {
+            window.alert("Please connect to Metamask.")
+        }
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum)
+            try {
+                // Request account access if needed
+                await ethereum.enable()
+                // Acccounts now exposed
+                web3.eth.sendTransaction({/* ... */ })
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            App.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+            // Acccounts always exposed
+            web3.eth.sendTransaction({/* ... */ })
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+
+        // Get the Account of the Wallet
+        const accounts = await web3.eth.getAccounts();
+        App.account = accounts[0];
+
+        // Co2 Emission Smart Contract
+        const Token = await $.getJSON('/contracts/EcoCreditToken.json')
+        App.contracts.Token = TruffleContract(Token)
+        App.contracts.Token.setProvider(App.web3Provider)
+        // Hydrate the smart contract with values from the blockchain
+        App.Token = await App.contracts.Token.deployed()
+
+        App.setLoading(true)
+        const newPrice = document.querySelector('#newPrice').value;
+        await App.Token.setTokenPrice(newPrice, { from: App.account })
+        window.location.href = '/dashboard'
+    },
+    initAllowance: async () => {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+        } else {
+            window.alert("Please connect to Metamask.")
+        }
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum)
+            try {
+                // Request account access if needed
+                await ethereum.enable()
+                // Acccounts now exposed
+                web3.eth.sendTransaction({/* ... */ })
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            App.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+            // Acccounts always exposed
+            web3.eth.sendTransaction({/* ... */ })
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+
+        // Get the Account of the Wallet
+        const accounts = await web3.eth.getAccounts();
+        App.account = accounts[0];
+
+        // Co2 Emission Smart Contract
+        const Token = await $.getJSON('/contracts/EcoCreditToken.json')
+        App.contracts.Token = TruffleContract(Token)
+        App.contracts.Token.setProvider(App.web3Provider)
+        // Hydrate the smart contract with values from the blockchain
+        App.Token = await App.contracts.Token.deployed()
+
+        App.setLoading(true)
+        const industryWalletID = document.querySelector('#industryWalletID').value;
+        const initTokens = document.querySelector('#initTokens').value;
+        await App.Token.initialAllowance(industryWalletID,initTokens, { from: App.account })
+        window.location.href = '/dashboard'
+    },
 
     setLoading: (boolean) => {
         App.loading = boolean
